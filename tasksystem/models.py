47efa62f7a7031fa7fecb8b8e52a9fcb4feca515
Timofii Khlistik <tima0323199@gmail.com>
2025-10-21 22:41:17 +0300

    update models.py TaskType and create new model Task

--- tasksystem/models.py
+++ tasksystem/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
-# Create your models here.
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Task(models.Model):
+    class Priority(models.TextChoices):
+        URGENT = "urgent", "Urgent"
+        HIGH = "high", "High"
+        LOW = "low", "Low"
+    name = models.CharField(max_length=255)
+    description =models.TextField()
+    deadline = models.DateTimeField()
+    is_complete = models.BooleanField(default=False)
+    priority = models.CharField(
+        max_length=10,
+        choices=Priority.choices,
+        default=Priority.LOW,
+    )
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE, related_name='tasks')
+
