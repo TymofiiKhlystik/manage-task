c14b57575111ae619d39882aff8045f57eb678ba
Timofii Khlistik <tima0323199@gmail.com>
2025-11-16 17:45:03 +0200

    Refactor: Move SECRET_KEY to environment variable

--- manage_task/settings.py
+++ manage_task/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 import os
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-ct(u+!vrn+tp@oelpv9w=d53@w-u!$@x%x1=6l*n@f_ksg7mx7"
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
